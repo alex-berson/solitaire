fac681d1a3f1deb071da9856ce3dc7afcc308b47
Alexander Berson <aberson@yahoo.com>
2025-01-14 16:33:55 +0100

    icon adjustment

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,45 @@
-const cacheName = 'cache-v2';
+const cacheName = 'cache-v3';
+
 const files = [
-  '/solitaire/',
-  'index.html',
-  'css/style.css',
-  'js/solitaire.js',
-  'images/arrows.svg',
-  'fonts/roboto-regular.woff2',
-  'fonts/roboto-bold.woff2'
+    '/solitaire/',
+    'index.html',
+    'css/style.css',
+    'js/solitaire.js',
+    'images/arrows.svg',
+    'fonts/roboto-regular.woff2',
+    'fonts/roboto-bold.woff2'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-      cache.addAll(files);
-      })
-  );
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            await cache.addAll(files);
+        })()
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
-      )
-    })
-  );
+    event.waitUntil(
+        (async () => {
+            const keys = await caches.keys();
+            await Promise.all(
+                keys
+                    .filter(key => key !== cacheName)
+                    .map(key => caches.delete(key))
+            );
+        })()
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    event.respondWith(
+        (async () => {
+            try {
+                return await fetch(event.request);
+            } catch {
+                return caches.match(event.request);
+            }
+        })()
+    );
 });
